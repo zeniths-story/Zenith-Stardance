f18adc50529e716443b1df867a1674a586544899
zeniths-story <mmathfam07@gmail.com>
2026-06-27 21:42:29 +0000

    v0.0.13

--- script.js
+++ script.js
@@ -4,81 +4,100 @@ setInterval(function () {
     document.querySelector("#timeOfEarth").innerHTML = new Date().toLocaleString();
 }, 1000);
 
+//clicktest
+document.getElementById("test").onclick = test();
+  
+function test() {
+    document.getElementById("test").innerHTML = "test success";
+};
+
+//opening and closing - does not work currently
+//original
+
+var welcomeScreen = document.querySelector("#wlcmescrn");
 
 
-//opening and closing
-var welcomeScreen = document.querySelector("#welcome");
+//close
+var welcomeScreenClose = document.querySelector("#wlcmeclose");
 
+welcomeScreenClose.addEventListener("click", function () {
+    closeWindow(welcomeScreen);
+});
+styles
 function closeWindow(element) {
     element.style.display = "none";
 }
+//open
+var welcomeScreenOpen = document.querySelector("#wlcmeopen");
+
+welcomeScreenOpen.addEventListener("click", function () {
+    openWindow(welcomeScreen);
+});
 
 function openWindow(element){
     element.style.display = "flex";
 }
 
-var welcomeScreenClose = document.querySelector("#welcomeclose");
-var welcomeScreenOpen = document.querySelector("#welcomeopen");
+//other/messing around
 
+//welcomeScreenClose.addEventListener("click", closeWindow(welcomeScreen));
+//welcomeScreenOpen.addEventListener("click", openWindow(welcomeScreen));
 
-//welcomeScreenClose.addEventListener("click", function () {
-    //closeWindow(welcomeScreen);
-//});
 
 //document.querySelector("#welcomeclose").addEventListener("click", function () {
     //closeWindow(document.querySelector("#welcome"));
-//});
-
-welcomeScreenClose.addEventListener("click", closeWindow(welcomeScreen));
-
-
-
-//welcomeScreenOpen.addEventListener("click", function () {
-    //openWindow(welcomeScreen);
 //});
 
 //document.querySelector("#welcomeopen").addEventListener("click", function () {
     //openWindow(document.querySelector("#welcome"));
 //});
 
-welcomeScreenOpen.addEventListener("click", openWindow(welcomeScreen));
+
 
 
 
 //dragging elements - buggy; needs fixed
 
-//dragElement(document.getElementById("welcome")); //
+//dragElement(document.getElementById("wlcmescrn"));
 
-//function dragElement(elmnt) {
-  //var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  //if (document.getElementById(elmnt.id + "header")) {
-   // document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  //} else {
-    //elmnt.onmousedown = dragMouseDown;
-  //}
+dragElement(welcomeScreen)
 
-  //function dragMouseDown(e) {
-    //e = e || window.event;
-    //e.preventDefault();
-    //pos3 = e.clientX;
-    //pos4 = e.clientY;
-    //document.onmouseup = closeDragElement;
-    //document.onmousemove = elementDrag;
- // }
+function dragElement(elmnt) {
+  
+  var currX = 0, currY = 0, initX = 0, initY = 0;
 
-  //function elementDrag(e) {
-   // e = e || window.event;
-   // e.preventDefault();
-    //pos1 = pos3 - e.clientX;
-    //pos2 = pos4 - e.clientY;
-    //pos3 = e.clientX;
-    //pos4 = e.clientY;
-    //elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-   // elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
- // }
+  if (document.getElementById(elmnt.id + "header")) {
 
-  //function closeDragElement() {
-    //document.onmouseup = null;
-    //document.onmousemove = null;
-  //}
-//}
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+
+  } else {
+
+    elmnt.onmousedown = dragMouseDown;
+
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    initX = e.clientX;
+    initY = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    currX = pos3 - e.clientX;
+    currY = pos4 - e.clientY;
+    initX = e.clientX;
+    initY = e.clientY;
+    elmnt.style.top = (elmnt.offsetTop - currY) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - currX) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
